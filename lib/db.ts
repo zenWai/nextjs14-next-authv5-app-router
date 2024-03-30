675f38b4184230e4aff97883d4ab96451741de6f
Presa <ivopresa@gmail.com>
2024-03-30 16:14:21 +0100

    "Prisma setup and postgreSQL"

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+// For prisma to not be affected by hot reload from nextjs
+if(process.env.NODE_ENV !== "production") globalThis.prisma = db;
