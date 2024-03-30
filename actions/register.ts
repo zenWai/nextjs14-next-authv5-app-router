ad9c65125ef955322582a409a91a3d612d88fe2d
Presa <ivopresa@gmail.com>
2024-03-30 16:28:48 +0100

    "Register flow" - hash password - confirm e-mail for uniqueness - create user

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import {RegisterSchema} from "@/schemas";
 import * as zod from "zod";
+import bcrypt from "bcrypt";
+import {db} from "@/lib/db";
 
 export const register = async (values: zod.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,5 +14,29 @@ export const register = async (values: zod.infer<typeof RegisterSchema>) => {
     }
   }
 
-  return {success: "Registration successful"}
+  const {email, password, name} = validatedFields.data;
+  // auto generate salted hash
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(existingUser) {
+    return {error: "Email already registered!"};
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  // TODO: send verification email
+
+  return {success: "User created!"}
 }
