cc73b158c9edbac5d0768596f37edb8400f90758
Presa <124523559+zenWai@users.noreply.github.com>
2024-03-30 15:48:30 +0100

    "Registration form" (#4)

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import {RegisterForm} from "@/components/auth/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <RegisterForm/>
+  )
+}
--- schemas/index.tsx
+++ schemas/index.tsx
@@ -6,3 +6,11 @@ export const LoginSchema = zod.object({
   }),
   password: zod.string().min(1, { message: "Password is required" }),
 })
+
+export const RegisterSchema = zod.object({
+  email: zod.string().email({
+    message: "Email is required",
+  }),
+  password: zod.string().min(6, { message: "Password is required" }),
+  name: zod.string().min(1, { message: "Name is required" }),
+})
