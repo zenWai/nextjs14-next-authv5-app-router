f3d443b2b18e3bf20347fda1a3596d8b3897b407
Presa <ivopresa@gmail.com>
2024-03-30 17:10:12 +0100

    Since not using prisma edge - middleware with auth.config

--- auth.ts
+++ auth.ts
@@ -1,9 +1,13 @@
+import authConfig from "@/auth.config";
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import {db} from '@/lib/db'
 
 export const {
-  handlers: { GET, POST },
+  handlers: {GET, POST},
   auth,
 } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(db),
+  session: {strategy: "jwt"},
+  ...authConfig,
 })
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
-import { auth } from "./auth"
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+
+const {auth} = NextAuth(authConfig);
 
 export default auth((req) => {
   console.log("ROUTE: ", req.nextUrl.pathname)
