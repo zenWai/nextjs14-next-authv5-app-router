0b0d0a84d0792b9e5c8424ca23d7d844447b95e3
Presa <ivopresa@gmail.com>
2024-04-01 23:54:37 +0200

    - oauth google and github

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,22 @@
 import {getUserByEmail} from "@/data/user";
 import {LoginSchema} from "@/schemas";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 import bcrypt from "bcryptjs";
 import Credentials from 'next-auth/providers/credentials'
 import type { NextAuthConfig } from "next-auth"
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+
+    }),
     Credentials({
 
       async authorize(credentials) {
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,19 +1,26 @@
 "use client"
 
 import {Button} from "@/components/ui/button";
+import {DEFAULT_LOGIN_REDIRECT} from "@/routes";
 import {FaGithub} from "react-icons/fa"
 import {FcGoogle} from "react-icons/fc"
-
+// to use inside client components without server actions
+import {signIn} from "next-auth/react"
 export function Social() {
+  //inside client components without server actions
+  const onClick = (provider:"google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+
+    })
+  }
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-
-        }}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5"/>
       </Button>
@@ -21,9 +28,7 @@ export function Social() {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-
-        }}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5"/>
       </Button>
