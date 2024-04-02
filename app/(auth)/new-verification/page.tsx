7665091c28d21d71253d6461032a228efa5b22f2
Presa <ivopresa@gmail.com>
2024-04-02 22:41:58 +0200

    "email verification feature" - Verification email with actions. - New page for email verification (`/new-verification`) to the list of public routes. - `NewVerificationForm` component to manage the email verification process, including reading token, firing newVerification action and displaying success or error messages. - `newVerification` action to handle the verification logic, including token validation, expiration check, user email verification, and token deletion after verification.

---
+++ app/(auth)/new-verification/page.tsx
@@ -0,0 +1,9 @@
+import {NewVerificationForm} from "@/components/auth/NewVerificationForm";
+
+export default function NewVerificationPage() {
+  return (
+    <div>
+      <NewVerificationForm/>
+    </div>
+  )
+}
