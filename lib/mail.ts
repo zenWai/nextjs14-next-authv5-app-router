8db3d1006c00c24faa3db71dda268a1ed7ed9c9f
Presa <ivopresa@gmail.com>
2024-04-05 22:37:36 +0200

    NEXT_PUBLIC_APP_URL env variable

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,6 +1,7 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
@@ -12,7 +13,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/new-password?token=${token}`;
+  const resetLink = `${domain}/new-password?token=${token}`;
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
@@ -23,7 +24,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/new-verification?token=${token}`;
+  const confirmLink = `${domain}/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
