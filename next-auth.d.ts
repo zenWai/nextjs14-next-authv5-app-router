32a44446a588d91bcd1a207d4564f40c75fb6087
Presa <ivopresa@gmail.com>
2024-04-05 21:30:23 +0200

    "next-auth.d.ts" - populate ExtendedUser with isOauth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,6 +4,7 @@ import NextAuth, { type DefaultSession } from 'next-auth';
 export type ExtendedUser = DefaultSession['user'] & {
   role: UserRole;
   isTwoFactorEnabled: boolean;
+  isOauth: boolean;
 };
 
 declare module 'next-auth' {
