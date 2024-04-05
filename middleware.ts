424ec86821a4af9e701f936c9ad1713ad6244163
Presa <124523559+zenWai@users.noreply.github.com>
2024-04-05 22:33:17 +0200

    "callbackUrl when logging, credentials&socials" (#16)

--- middleware.ts
+++ middleware.ts
@@ -23,7 +23,13 @@ export default auth((req) => {
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL('/login', nextUrl));
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
   }
 
   return undefined;
