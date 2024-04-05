55192110faa5ef7dba44a3525df4d78a2034605f
Presa <ivopresa@gmail.com>
2024-04-05 21:29:56 +0200

    "SettingsSchema"

--- schemas/index.tsx
+++ schemas/index.tsx
@@ -1,3 +1,4 @@
+import { UserRole } from '@prisma/client';
 import * as zod from 'zod';
 
 export const LoginSchema = zod.object({
@@ -25,3 +26,39 @@ export const ResetPasswordSchema = zod.object({
 export const NewPasswordSchema = zod.object({
   password: zod.string().min(6, { message: 'Minimum of 6 characters required' }),
 });
+
+export const SettingsSchema = zod
+  .object({
+    name: zod.optional(zod.string().min(1)),
+    isTwoFactorEnabled: zod.optional(zod.boolean()),
+    role: zod.enum([UserRole.ADMIN, UserRole.USER]),
+    email: zod.optional(zod.string().email()),
+    password: zod.optional(zod.string().min(6)),
+    newPassword: zod.optional(zod.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: 'New Password is required!',
+      path: ['newPassword'],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: 'Password is required!',
+      path: ['password'],
+    }
+  );
