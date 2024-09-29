51d8ecfb49caf940d7a6314332bcd47181d7af75
Presa <ivopresa@gmail.com>
2024-09-29 04:28:52 +0200

    RoleGate was a open gate. Leaking content. F
    - Changed /admin/page.tsx to server page and refactored to address that
    
    ** RoleGate.tsx**
    - It is now a server component and refactored to address that
    - It is not leaking the secret content in the source files
    
    **admin-only-rh-and-sa.tsx**:
    - created from the client functionality present @ /admin/page.tsx
    - Changed mentions from Api to Route Handler to address the naming convention on next.js App Router
    - onRouteHandlerClick with better error handling
    
    **/api/admin/route.ts**
    - Route Handler correctly handling possible errors
    - Using the NextResponse.json() convenience method

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,35 +1,11 @@
-'use client';
 import { UserRole } from '@prisma/client';
-import { toast } from 'sonner';
 
-import { admin } from '@/actions/admin';
+import { AdminOnlyRhAndSa } from '@/app/(protected)/_components/admin-only-rh-and-sa';
 import { RoleGate } from '@/components/auth/RoleGate';
 import { FormSuccess } from '@/components/form-messages/FormSuccess';
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 export default function AdminPage() {
-  const onApiRouteClick = () => {
-    fetch('/api/admin').then((response) => {
-      if (response.ok) {
-        toast.success('Allowed API Route');
-      } else {
-        toast.error('Forbidden API Route');
-      }
-    });
-  };
-
-  const onServerActionClick = () => {
-    admin().then((data) => {
-      if (data.error) {
-        toast.error(data.error);
-      }
-
-      if (data.success) {
-        toast.success(data.success);
-      }
-    });
-  };
   return (
     <Card className='w-[600px]'>
       <CardHeader>
@@ -45,14 +21,7 @@ export default function AdminPage() {
           <FormSuccess message='You are allowed to see this content' />
           <p> This is a example of secret content </p>
         </RoleGate>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
-          <p className='text-sm font-medium'>Admin-only API Route</p>
-          <Button onClick={onApiRouteClick}>Click to test</Button>
-        </div>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
-          <p className='text-sm font-medium'>Admin-only Server Action</p>
-          <Button onClick={onServerActionClick}>Click to test</Button>
-        </div>
+        <AdminOnlyRhAndSa />
       </CardContent>
     </Card>
   );
--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -4,12 +4,15 @@ import { NextResponse } from 'next/server';
 import { currentSessionRole } from '@/lib/auth-utils';
 
 export async function GET() {
-  const role = await currentSessionRole();
+  try {
+    const role = await currentSessionRole();
 
-  if (role === UserRole.ADMIN) {
-    return new NextResponse(null, {
-      status: 200,
-    });
+    if (role === UserRole.ADMIN) {
+      return NextResponse.json({ message: 'Allowed RH call' }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'Forbidden RH call' }, { status: 403 });
+    }
+  } catch (error) {
+    return NextResponse.json({ message: `Internal Server Error: ${error}` }, { status: 500 });
   }
-  return new NextResponse(null, { status: 403 });
 }
