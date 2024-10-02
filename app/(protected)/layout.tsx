7bc5acf313fd164e3d612724b44f3bf705753bc8
Presa <ivopresa@gmail.com>
2024-10-02 07:36:09 +0200

    - SessionProvider is not used on root layout, navbar refactored to have UserAvatarMenu serverside for session, clientPage refactored and created client component for demo

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,4 +1,6 @@
-import { Navbar } from '@/app/(protected)/_components/Navbar';
+import { NavigationMenu } from '@/components/navbar/NavigationMenu';
+import { UserAvatarMenu } from '@/components/navbar/UserAvatarMenu';
+import { Navbar } from '@/components/navbar/Navbar';
 
 export default function ProtectedLayout(props: { children: React.ReactNode }) {
   return (
@@ -6,7 +8,10 @@ export default function ProtectedLayout(props: { children: React.ReactNode }) {
       className='flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
         from-sky-400 to-blue-800'
     >
-      <Navbar />
+      <Navbar>
+        <NavigationMenu />
+        <UserAvatarMenu />
+      </Navbar>
       {props.children}
     </div>
   );
