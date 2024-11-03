be99ecd963701d471ea20ce8798dd436def3d50a
Presa <ivopresa@gmail.com>
2024-11-03 22:33:43 +0100

    **Playwright testing**
    - Changed env.example
    - removed eslint no-console on /e2e-tests
    - gitignore updated
    - auth.config.ts with updated env var

--- auth.config.ts
+++ auth.config.ts
@@ -10,12 +10,12 @@ import { getUserByEmail } from '@/data/user';
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
     }),
     Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
